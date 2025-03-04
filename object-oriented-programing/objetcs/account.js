5362e7b3f9d7aa73943201f317ae636acc37e4c0
Max Pacheco <p4ch1ss@gmail.com>
2025-03-03 19:01:49 -0600

    chore(objetcs): add account exercise

---
+++ object-oriented-programing/objetcs/account.js
@@ -0,0 +1,64 @@
+/*
+Estás diseñando un sistema básico para gestionar cuentas bancarias de clientes.
+
+Cada cuenta bancaria tendrá un identificador único, un nombre del titular, y un saldo.
+
+Tu tarea es crear una función createAccount que genere un objeto de cuenta bancaria.
+
+El objeto debe ser creado por medio de un function declaration, con todas las propiedades y métodos deben estar definidos dentro de la 
+función usando this.
+
+Propiedades de la cuenta bancaria (dentro de la función usando this):
+
+id: identificador único de la cuenta.
+holderName: nombre del titular de la cuenta.
+balance: saldo de la cuenta.
+Métodos de la cuenta bancaria (dentro de la función usando this):
+
+deposit(amount): incrementa el saldo de la cuenta.
+withdraw(amount): disminuye el saldo de la cuenta si hay suficientes fondos.
+transfer(toAccount, amount): transfiere fondos de la cuenta actual a otra cuenta si hay suficientes fondos disponibles.
+*/
+
+
+
+function Account(id, name, balance = 0) {
+this.id = id
+this.name = name
+this.balance = balance
+this.deposit = (amount) => {
+    this.balance = this.balance + amount
+}
+this.withdraw = (amount) => {
+   if (this.balance >= amount) {
+    this.balance = this.balance - amount
+    return amount
+} 
+
+return null 
+}
+
+this.transfer = (toAccount, amount) => {
+    const money = this.withdraw(amount)
+
+    if (money) {
+    toAccount.deposit(money)
+    return toAccount
+    }
+
+    return null 
+}
+
+
+return this
+}
+
+const acc1 = new Account (1, "Max", 50000)
+acc1.deposit(2000)
+acc1.withdraw(10000)
+
+const acc2 = new Account (2, "Alberto")
+
+console.log(acc1.transfer(acc2, 5000))
+
+console.log(acc1)
